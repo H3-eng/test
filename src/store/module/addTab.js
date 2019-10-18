724815d6f1b46507005a534a86b48aac9c65c5a6
jianhui0805 <huang.jianhui@southgis.com>
2019-10-18 10:39:22 +0800

    通过相对路径加载页面

--- src/store/module/addTab.js
+++ src/store/module/addTab.js
@@ -1,19 +1,5 @@
 import * as types from '../type'
 import Vue from 'vue'
-
-function checkURL(URL) {
-  let str = URL;
-  //判断URL地址的正则表达式为:http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?
-  //下面的代码中应用了转义字符"\"输出一个字符"/"
-  // eslint-disable-next-line no-useless-escape
-  let Expression = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
-  let objExp = new RegExp(Expression);
-  if (objExp.test(str) == true) {
-    return true;
-  } else {
-    return false;
-  }
-}
 const state = {
   active: "", //当前选中菜单
   tabList:[]
@@ -46,19 +32,11 @@ const mutations = {
     let title = value.name;
     let component = null;
     if (state.tabList.filter(f => f.name == value.moduleId) == 0) {
-      if (checkURL(index)) {
-        let templ = '<div style="height: 100%">' +
-          '<iframe style="display: block;width:100%;height: 100%;border:0" src=' + index + '></iframe></div>'
-        component = Vue.component('sg-iframe', {
-          template: templ
-        })
-      } else {
-        if(index===''){
-          // alert('功能尚未发布')
-          return false
-        }
-        component = resolve => require([`@/pages/${index}`], resolve)
-      }
+      let templ = '<div style="height: 100%">' +
+        '<iframe style="display: block;width:100%;height: 100%;border:0" src=' + index + '></iframe></div>'
+      component = Vue.component('sg-iframe', {
+        template: templ
+      })
       state.tabList.push({
         label: title,
         name: value.moduleId,
