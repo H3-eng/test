af66fee5da0384a593657de466864af89754c932
jiahui.xie <1336294280@qq.com>
2019-12-04 14:48:06 +0800

    test

--- src/component/menu/submenu.vue
+++ src/component/menu/submenu.vue
@@ -24,9 +24,13 @@
         </div>
     </sg-submenu>
     <sg-menu-item :name="item.moduleId" v-else @click.native="addTab(item)">
-        <img :src="item.imageUrl" class="icon">
-        <span>{{item.name}}</span>
+      <img :src="item.imageUrl" class="icon">
+      <span>{{item.name}}</span>
     </sg-menu-item>
+      <sg-menu-item :name="item.moduleId" v-else @click.native="addTab(item)">
+          <img :src="item.imageUrl" class="icon">
+          <span>{{item.name}}</span>
+      </sg-menu-item>
   </div>
 </template>
 
